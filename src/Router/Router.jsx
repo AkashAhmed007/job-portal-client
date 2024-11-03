cd73c395844fb47f34645b5ed2767be63264c114
Akash Ahmed <akashahmedlikhon.007@gmail.com>
2024-11-03 22:24:30 +0600

    error page created

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,9 +1,11 @@
 import { createBrowserRouter } from "react-router-dom";
 import RootLayout from '../Root/RootLayout'
+import ErrorPage from "../Pages/ErrorPage";
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <RootLayout></RootLayout>
+    element: <RootLayout></RootLayout>,
+    errorElement: <ErrorPage />,
   },
 ]);
 
